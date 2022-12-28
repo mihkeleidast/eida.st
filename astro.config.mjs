5693b76e64a731b1d5ca1a8bf43c160a5aaac018
Mihkel Eidast <mihkel@eida.st>
2022-12-28 15:16:12 +0200

    feat: taylor swift vinyl collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [image()],
-    trailingSlash: "always",
+  integrations: [image(), mdx()],
+  trailingSlash: "always"
 });
