00a43b827da7bab5f5809a1f1b25a7b9ec1d5c12
Mihkel Eidast <mihkel@eida.st>
2025-07-08 21:35:26 +0300

    astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), mdx()],
+  integrations: [mdx()],
   trailingSlash: "always"
 });
